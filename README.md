363ceb954cc8459487e9375074a913d3900faa58
Mitch Anderson <mitch.anderson@undeadops.xyz>
2022-03-21 14:35:54 -0600

    Adding a readme and other updates

---
+++ README.md
@@ -0,0 +1,13 @@
+# dotfiles
+
+My Working dotfiles directory. 
+
+Containing configs for neovim, etc..
+
+### Installation
+
+```bash
+brew install stow
+```
+
+
--- install.sh
+++ install.sh
@@ -1,27 +1,13 @@
 set -e
-
-# Change shell to zsh
-chsh -s $(which zsh)
-
-echo -n "Symlinking dotfiles..."
-#Symlink .zshrc and .vimrc
-mkdir -p $HOME/.config/nvim/autoload
-ln -f -s $HOME/.dotfiles/vim/vimrc $HOME/.config/nvim/init.vim
-ln -f -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
-ln -f -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
-ln -f -s $HOME/.dotfiles/zsh/zshenv $HOME/.zshenv
-ln -f -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+# Checkout Repo into base home folder $HOME/dotfiles
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+brew install nvim ripgrep stow
 
-nvim +PlugInstall +q
+# Setup sym links
+cd $HOME/dotfiles && \
+	stow zsh && \
+	stow nvim
 
-
-# ZSH Suggestions
-
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.dotfiles/zsh-autosuggestions
