ebaf7109dd6419cfbe0714c96a4945edc8cb3184
Mitch Anderson <mitch@undeadops.xyz>
2023-07-05 16:39:26 -0600

    fix: Updating nvim config neotree

--- nvim/.config/nvim/lua/custom/plugins/neotree.lua
+++ nvim/.config/nvim/lua/custom/plugins/neotree.lua
@@ -2,14 +2,14 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	version = "v2.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require('neo-tree').setup {}
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v2.x",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  }
 }
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
